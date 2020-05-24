6ab509607098d1f08acf9d473af0e43b6182f256
AltuninAndrew <al_andr_vl@mail.ru>
2020-05-24 23:11:49 +0300

    Implemented Evaluation form

--- src/UserPages/EvaluationUserPage/EvaluationFormComponent/EvaluationFormComponent.jsx
+++ src/UserPages/EvaluationUserPage/EvaluationFormComponent/EvaluationFormComponent.jsx
@@ -1,36 +1,70 @@
 import React from 'react';
 import classes from "./EvaluationFormComponent.module.css";
-import {Field} from "redux-form";
+import {Field, reduxForm, reset} from "redux-form";
+import {required} from "../../../Validators/validators";
 
-const CriterionElement = () => {
+let criterions = ["Качество работы", "Ловкость","Чутьё"];
+
+const CriterionElement = (props) => {
     return (
         <div className={classes.crit_element_wrapper}>
             <div className={classes.crit_name_txt}>
-                Качество работы
+                {props.name}
             </div>
-            <input
+
+            <Field
+                component={"input"}
+                type={"number"}
                 className={classes.input_score}
                 placeholder="1"
                 step="1" min="1" max="10"
-                type="number"
+                validate={[required]}
+                name={props.name}
             />
-
         </div>
     );
 };
 
+const EvaluationForm = (props) =>{
+    let criterionElements = criterions.map((crit, index) => (<CriterionElement name={crit} key={index}/>));
+    return(
+        <form onSubmit={props.handleSubmit}>
+            <div className={classes.main_wrapper}>
+                <div className={classes.criterions_wrapper}>
+                    {criterionElements}
+                </div>
+                <div>
+                    <button>Следующий</button>
+                </div>
+            </div>
+        </form>
+    );
+};
+
+const afterSubmit = (result, dispatch) =>{
+
+};
+
+const EvaluationReduxForm = reduxForm({form: 'addCriterion', onSubmitSuccess: afterSubmit})(EvaluationForm);
+
+
 const EvaluationFormComponent = () => {
+    const onSubmitForm = (formData) => {
+        let raw = JSON.stringify(formData).split(/"(.*?)"/).filter(e=>e!=="{" && e!=="}" && e!=="," && e!==":");
+        let objs=[];
+        for(let i =0; i<raw.length;i++){
+            if(i%2===0) {
+                objs.push({
+                    name:raw[i],
+                    score:raw[i+1],
+                })
+            }
+        }
+        console.log(objs);
+    };
+
     return (
-        <div className={classes.main_wrapper}>
-            <div className={classes.criterions_wrapper}>
-                <CriterionElement/>
-                <CriterionElement/>
-                <CriterionElement/>
-            </div>
-            <div>
-                <button>Следующий</button>
-            </div>
-        </div>
+        <EvaluationReduxForm onSubmit={onSubmitForm}/>
     );
 };
 
